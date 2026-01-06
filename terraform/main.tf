1d03fab5de41fff4418a91e199196c264ddf0416
Gabriel Murakami <gabriel.m.alves42@gmail.com>
2026-01-06 20:43:47 -0300

    add traefik

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,43 @@ resource "kubectl_manifest" "datadog_agent" {
   yaml_body  = file("${path.module}/../k8s/datadog-agent.yaml")
   depends_on = [kubectl_manifest.namespace]
 }
+
+resource "kubectl_manifest" "traefik_middlewares" {
+  yaml_body = file("${path.module}/../k8s/traefik/middlewares.yaml")
+
+  depends_on = [
+    kubectl_manifest.namespace
+  ]
+}
+
+resource "kubectl_manifest" "traefik_forward_auth" {
+  yaml_body = file("${path.module}/../k8s/traefik/forward-auth.yaml")
+
+  depends_on = [
+    kubectl_manifest.traefik_middlewares
+  ]
+}
+
+resource "kubectl_manifest" "traefik_public_route" {
+  yaml_body = file("${path.module}/../k8s/traefik/public-route.yaml")
+
+  depends_on = [
+    kubectl_manifest.traefik_forward_auth
+  ]
+}
+
+resource "kubectl_manifest" "traefik_auth_route" {
+  yaml_body = file("${path.module}/../k8s/traefik/auth-route.yaml")
+
+  depends_on = [
+    kubectl_manifest.traefik_forward_auth
+  ]
+}
+
+resource "kubectl_manifest" "traefik_api_route" {
+  yaml_body = file("${path.module}/../k8s/traefik/api-route.yaml")
+
+  depends_on = [
+    kubectl_manifest.traefik_forward_auth
+  ]
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,9 +4,26 @@ terraform {
       source  = "gavinbunney/kubectl"
       version = "~> 1.14"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
+
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 3.1"
+    }
   }
 }
 
 provider "kubectl" {
   load_config_file = true
 }
+
+provider "kubernetes" {
+  config_path    = pathexpand("~/.kube/config")
+  config_context = "minikube"
+}
+
+provider "helm" {}
